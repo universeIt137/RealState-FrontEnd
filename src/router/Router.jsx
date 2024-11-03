7acc296f4eaeb266dc2a80355d09fd4b0ff75491
ishanrana363 <ishanrana363@gmail.com>
2024-11-03 18:32:42 +0600

    form download section done

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,9 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layout/Layout";
+import BookingFrom from "../components/booking-from/BookingFrom";
 
 export const router = createBrowserRouter([
     {
         path : "/",
-        element : <Layout></Layout>
-    }
+        element : <Layout></Layout>,
+        children : [
+            {
+                path : "/booking-form",
+                element : <BookingFrom></BookingFrom>
+            },
+        ]
+    },
+
+    
 ]);
