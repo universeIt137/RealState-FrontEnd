054498d4f50aff8ecd3c38ba1128a2ff8f444e1e
ashikur <ashikur1602@gmail.com>
2024-11-21 11:54:41 +0600

    working on apply on Admin panel

---
+++ src/pages/adminSide/careerRelatedPages/ManageApply.jsx
@@ -0,0 +1,94 @@
+import { useQuery } from '@tanstack/react-query';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import useAxiosPublic from '../../../hooks/useAxiosPublic';
+
+const ManageApply = () => {
+    const axiosPublic = useAxiosPublic();
+
+
+    const { data: allData = [], refetch } = useQuery({
+        queryKey: ['careerData'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/career');
+            return res.data;
+        }
+    })
+
+
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: 'You won’t be able to revert this!',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'Cancel',
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosPublic
+                    .delete(`/career/${id}`)
+                    .then((res) => {
+                        if (res) {
+                            Swal.fire({
+                                title: 'Deleted!',
+                                text: 'Data has been deleted.',
+                                icon: 'success',
+                            });
+                            refetch();
+                        }
+
+                    })
+                    .catch((err) => {
+                        console.log(err);
+
+                    });
+            }
+        });
+    };
+
+
+    return (
+        <div className="overflow-x-auto w-full my-5">
+            <p className="text-2xl font-bold text-center mb-2">Manage Career</p>
+            <table className="min-w-full bg-white border border-gray-300">
+                <thead>
+                    <tr>
+                        <th className="px-4 py-2 border">Job Title</th>
+                        <th className="px-4 py-2 border">Salary</th>
+                        <th className="px-4 py-2 border">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        allData?.map((content) => (
+                            <tr key={content?._id} className="text-center">
+                                <td className="px-4 py-2 border font-semibold">{content?.job_title}</td>
+                                <td className="px-4 py-2 border font-semibold">{content?.salary}</td>
+                                
+                                <td className="px-4 py-2 border">
+                                    <button
+
+                                        className="px-2 py-1 bg-blue-500 text-white rounded mr-2"
+                                    >
+                                        <Link to={`/dashboard/update-career/${content?._id}`}>Update</Link>
+                                    </button>
+                                    <button
+                                        onClick={() => handleDelete(content?._id)}
+                                        className="px-2 py-1 bg-red-500 text-white rounded"
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default ManageApply;
+
