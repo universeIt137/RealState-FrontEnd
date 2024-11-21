3e1c39dd3ba52fa4aac5f88eb6bdf72873c8dfdd
ishanrana363 <ishanrana363@gmail.com>
2024-11-21 11:52:31 +0600

    video gallery crud operation completed

---
+++ src/pages/adminSide/imgGalleryRelatedPage/UpdateImg.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpdateImg = () => {
+  return (
+    <div>
+      <h1>UpdateImg</h1>
+    </div>
+  )
+}
+
+export default UpdateImg
---
+++ src/pages/adminSide/imgGalleryRelatedPage/UploadImg.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UploadImg = () => {
+    return (
+        <div>
+            <h1>UploadImg</h1>
+        </div>
+    )
+}
+
+export default UploadImg
