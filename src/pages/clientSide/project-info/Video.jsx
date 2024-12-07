88376ce9df0fc8da4e2075b1ae06794dd63518eb
ishanrana363 <ishanrana363@gmail.com>
2024-12-07 15:41:30 +0600

    current project status completed

--- src/pages/clientSide/project-info/Video.jsx
+++ src/pages/clientSide/project-info/Video.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import useAxiosPublic from '../../../hooks/useAxiosPublic';
 import { useQuery } from '@tanstack/react-query';
 import ProjectFeatureVideoGallery from '../home-page/ProjectFeatureVideoGallery';
+import { Helmet } from 'react-helmet-async';
 
 const Video = () => {
     window.scrollTo(0, 0);
@@ -19,6 +20,9 @@ const Video = () => {
 
     return (
         <div className=' w-11/12 mx-auto my-24 ' >
+            <Helmet>
+                <title>Amer Thikana | Project Video </title>
+            </Helmet>
             {/* Video Section */}
             <div className="bg-white border-2 rounded-lg shadow-lg lg:px-6 p-3 my-4 lg:my-8 lg:space-y-4">
                 <h2 className="lg:text-2xl text-[15px] font-semibold text-gray-800">প্রকল্প ভিডিও</h2>
